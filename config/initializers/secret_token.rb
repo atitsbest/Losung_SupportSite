b90ea89bce81f1a9e8d53bcfbaf11c87bdd1eea0
Stephan Meissner <atitsbest@gmail.com>
2010-10-26 20:58:23 +0200

    Einfache Webseite in eine Rails-App umgewandelt.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site::Application.config.secret_token = '24a5551419a2080c46f3403c732b5b1712b8814fd433a89610b603de1c8be88b01cbb13901a3a0272611b48c87eaeee40e1173b9b61c3c15f4bb1d0567c415fb'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Site::Application.routes.draw do
+  get "home/index"
+
+  root :to => 'home#index'
+end
